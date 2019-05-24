de8f675843ebd8b787b2ed4879268f54be766f4a
Tim Ramsier <tim.ramsier@dictionary.com>
2019-05-24 13:49:21 -0700

    Update Methods and Comments
    - Add comments to explain each method
    - Add ability to get node count of trie

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * A Node in the trie containing meta information and the character value.  If terminate is true
+ * it indicates that a complete word can be created from the Node back to the top of the tree.
+ */
 class Node {
   constructor({ char, children, terminate, parent } = {}) {
     this.char = char;
@@ -12,6 +16,10 @@ class AutoComplete {
     this.head = new Node({ char: "" });
   }
 
+  /**
+   * Returns an array of all of the leafs of the trie
+   * @param {*} node
+   */
   findLeafs(node) {
     if (!node.children.length) return [node];
     if (node.terminate)
@@ -26,12 +34,18 @@ class AutoComplete {
       .reduce((acc, val) => acc.concat(val), []);
   }
 
+  /**
+   * Add and/or update tree with the necessary nodes and terminates
+   * @param {*} node
+   * @param {*} chars
+   */
   addCharactersToNode(node, chars) {
     if (!chars.length) return;
     const char = chars.shift();
     let charNode = [...node.children.filter(child => child.char === char)][0];
     if (!charNode) {
       charNode = new Node({ char, terminate: chars.length < 1, parent: node });
+      node.score += 1;
       node.children.push(charNode);
     } else if (!charNode.terminate) {
       charNode.terminate = chars.length < 1;
@@ -39,21 +53,52 @@ class AutoComplete {
     this.addCharactersToNode(charNode, chars);
   }
 
+  /**
+   * Add a single new word to the trie
+   * @param {*} word 
+   */
   addWord(word) {
     const chars = [...word];
     this.addCharactersToNode(this.head, chars);
   }
 
+  /**
+   * Add an array of new words to the trie
+   * @param {*} wordArray 
+   */
   addWords(wordArray) {
     wordArray.forEach(word => this.addWord(word));
   }
 
+  /**
+   * Count all of the nodes from the provided node (default is the head)
+   * @param {*} node 
+   */
+  getNodeCount(node = this.head) {
+    let count = 1;
+    if (!node.children.length) return count;
+    count += node.children
+      .map(child => this.getNodeCount(child))
+      .reduce((acc, curr) => acc + curr);
+    return count;
+  }
+
+  /**
+   * Get a word from a leaf or terminate Node
+   * @param {*} node 
+   * @param {*} str 
+   */
   getWord(node, str = "") {
     if (!node.parent) return str;
     return this.getWord(node.parent, `${node.char}${str}`);
   }
 
-  findNode(parentNode, chars) {
+  /**
+   * Traverse the tree to find the node from an array of characters
+   * @param {*} chars 
+   * @param {*} parentNode 
+   */
+  findNode(chars, parentNode = this.head) {
     if (!chars.length) return parentNode;
     const char = chars.shift();
     const node = [
@@ -61,9 +106,13 @@ class AutoComplete {
     ][0];
     // eslint-disable-next-line consistent-return
     if (!node) return;
-    return this.findNode(node, chars);
+    return this.findNode(chars, node);
   }
 
+  /**
+   * Find all words that begin with the value of the provided string
+   * @param {*} str 
+   */
   findWords(str) {
     const chars = [...str];
     const node = this.findNode(this.head, chars);
